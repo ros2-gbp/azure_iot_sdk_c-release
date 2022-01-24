c61f14b01801bc4f0b00f613e530f9fb0161f740
Tim Clephas <tim.clephas@nobleo.nl>
2022-01-24 09:00:22 +0100

    Imported upstream version '1.9.0' of 'upstream'

--- uamqp/deps/azure-c-shared-utility/adapters/tlsio_schannel.c
+++ uamqp/deps/azure-c-shared-utility/adapters/tlsio_schannel.c
@@ -388,6 +388,16 @@ static void send_client_hello(TLS_IO_INSTANCE* tls_io_instance)
                 }
             }
         }
+
+        if (init_security_buffers[0].pvBuffer != NULL)
+        {
+            FreeContextBuffer(init_security_buffers[0].pvBuffer);
+        }
+
+        if (init_security_buffers[1].pvBuffer != NULL)
+        {
+            FreeContextBuffer(init_security_buffers[1].pvBuffer);
+        }
     }
 }
 
@@ -825,6 +835,18 @@ static void on_underlying_io_bytes_received(void* context, const unsigned char* 
                     }
                     break;
                 }
+
+                if (output_buffers[0].pvBuffer != NULL)
+                {
+                    FreeContextBuffer(output_buffers[0].pvBuffer);
+                    output_buffers[0].pvBuffer = NULL;
+                }
+
+                if (output_buffers[1].pvBuffer != NULL)
+                {
+                    FreeContextBuffer(output_buffers[1].pvBuffer);
+                    output_buffers[1].pvBuffer = NULL;
+                }
             }
             else if (tls_io_instance->tlsio_state == TLSIO_STATE_OPEN)
             {
@@ -964,6 +986,18 @@ static void on_underlying_io_bytes_received(void* context, const unsigned char* 
                                 tls_io_instance->tlsio_state = TLSIO_STATE_RENEGOTIATE;
                             }
                         }
+                    }
+
+                    if (output_buffers[0].pvBuffer != NULL)
+                    {
+                        FreeContextBuffer(output_buffers[0].pvBuffer);
+                        output_buffers[0].pvBuffer = NULL;
+                    }
+
+                    if (output_buffers[1].pvBuffer != NULL)
+                    {
+                        FreeContextBuffer(output_buffers[1].pvBuffer);
+                        output_buffers[1].pvBuffer = NULL;
                     }
                     break;
                 }
